feecab6fe22128981ac61a95e40663a30a81dc9e
avin-c <avin.chiu@student.tdsb.on.ca>
2026-06-03 22:18:12 -0400

    Initial commit with Vite and React

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <header>
+        <h1>Avin Chiu</h1>
+      </header>
+      <main>
+        <p>This is my existing project content!</p>
+      </main>
+    </>
+  );
+}
+
+export default App
