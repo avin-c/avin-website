59c1dc660fc330028f6ecb0416ac11eab526e40d
avin-c <avin.chiu@student.tdsb.on.ca>
2026-06-13 21:43:27 -0400

    removed the basic vite assets and added my own

---
+++ src/assets/watermelon.tsx
@@ -0,0 +1,38 @@
+export function Sprite() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={16}
+      height={16}
+      viewBox="0 0 16 16"
+      shapeRendering="crispEdges"
+    >
+      <rect x={8} y={0} width={1} height={1} fill="#982f2f" />
+      <rect x={7} y={1} width={2} height={1} fill="#982f2f" />
+      <rect x={7} y={2} width={3} height={1} fill="#982f2f" />
+      <rect x={6} y={3} width={4} height={1} fill="#982f2f" />
+      <rect x={5} y={4} width={6} height={1} fill="#982f2f" />
+      <rect x={4} y={5} width={8} height={1} fill="#982f2f" />
+      <rect x={3} y={6} width={10} height={1} fill="#982f2f" />
+      <rect x={3} y={7} width={11} height={1} fill="#982f2f" />
+      <rect x={2} y={8} width={4} height={1} fill="#982f2f" />
+      <rect x={6} y={8} width={1} height={1} fill="#2f1919" />
+      <rect x={7} y={8} width={5} height={1} fill="#982f2f" />
+      <rect x={12} y={8} width={1} height={1} fill="#2f1919" />
+      <rect x={13} y={8} width={2} height={1} fill="#982f2f" />
+      <rect x={2} y={9} width={14} height={1} fill="#982f2f" />
+      <rect x={1} y={10} width={8} height={1} fill="#982f2f" />
+      <rect x={9} y={10} width={1} height={1} fill="#2f1919" />
+      <rect x={10} y={10} width={5} height={1} fill="#982f2f" />
+      <rect x={15} y={10} width={1} height={1} fill="#1e6222" />
+      <rect x={1} y={11} width={3} height={1} fill="#1e6222" />
+      <rect x={4} y={11} width={10} height={1} fill="#982f2f" />
+      <rect x={14} y={11} width={2} height={1} fill="#1e6222" />
+      <rect x={3} y={12} width={2} height={1} fill="#1e6222" />
+      <rect x={5} y={12} width={8} height={1} fill="#982f2f" />
+      <rect x={13} y={12} width={3} height={1} fill="#1e6222" />
+      <rect x={4} y={13} width={10} height={1} fill="#1e6222" />
+      <rect x={5} y={14} width={7} height={1} fill="#1e6222" />
+    </svg>
+  );
+}
