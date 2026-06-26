ca394002e1a84e1c7878aa6bfce51fd38c1e7a1a
avin-c <avin.chiu@student.tdsb.on.ca>
2026-06-26 10:56:32 -0400

    Updated about with grammatical changes, and flex gaps instead of line breaks

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,18 +6,13 @@ function About ({name, id}){
         <div id = {id} className="content">
             <h3 className="header">{name}</h3>
             <p>Hi! I'm a high school student from Canada passionate about engineering, and I love building things! I enjoy building both hardware and software, like this website!
-            My love for making things started when I was little, from building LEGOs to making cardboard crafts. Now i'm into webdev, 3d printing, and electronics, and woodworking.
-            <br></br>
-            <br></br>
-
-            i joined hack club <a href="https://construct.hackclub.com/">construct</a> in march 2026 because i wanted a 3d printer. unfortunately i didnt get one because i was too busy with school to complete the hours, but i had a lot of fun in the process! this website is my first official hack club project! 
-            <br></br>
-            <br></br>
-            when i'm bored, i like to play <a href="https://www.chess.com/member/malkinaw">chess</a>, play the violin, woodwork, and eat ice cream (my fav flavor is vanilla).
-
-            <br></br>
-            <br></br>
-            check out the rest of my site!
+            My love for making things started when I was little, from building LEGOs to making cardboard crafts. Now I'm into webdev, 3d printing, and electronics, and woodworking.
+            </p>
+            <p>
+            I joined Hack Club <a href="https://construct.hackclub.com/">Construct</a> in March 2026 because I wanted a 3D printer. Unfortunately I didn't get one because I was too busy with school to complete the hours, but I had a lot of fun in the process! This website is my first official Hack Club project! 
+            </p>
+            <p>
+            When I'm bored, I like to play <a href="https://www.chess.com/member/malkinaw">chess</a>, play the violin, woodwork, and eat ice cream (my fav flavor is key lime pie).
             </p>
         </div>
     ); 
