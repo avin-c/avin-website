48eb1f73b080e25e562ff7344886d6350a49c4d7
avin-c <avin.chiu@student.tdsb.on.ca>
2026-06-30 21:54:23 -0400

    Created a signature section in the guestbook entries to display the signature/doodle from the form.

--- src/components/GuestbookDisplay.jsx
+++ src/components/GuestbookDisplay.jsx
@@ -1,9 +1,16 @@
 import { useEffect, useState } from "react";
 import { supabase } from "../supabase.js";
+import { getStroke } from 'perfect-freehand'
 
-export default function GuestbookList() {
+export default function GuestbookDisplay(props) {
   const [entries, setEntries] = useState([]);
 
+  function getSvgPathFromStroke (stroke){
+    return props.getSvgPathFromStroke(stroke);
+  }
+  function strokeToPath(stroke){
+    return props.strokeToPath(stroke);
+  }
   useEffect(() => {
     async function fetchEntries() {
       const { data, error } = await supabase
@@ -22,6 +29,7 @@ export default function GuestbookList() {
     fetchEntries();
   }, []);
   console.log(entries);
+
   return (
     <div className = "guestbookdisplay">
       {entries.map((entry) => {
@@ -29,13 +37,33 @@ export default function GuestbookList() {
         let dateObj = new Date(timestamp);
         const formattedDate = new Intl.DateTimeFormat('en-US', {
         dateStyle: 'medium',
+        timeStyle: 'short'
         }).format(dateObj);
         return (
         <div key={entry.id} className = "guestbookEntry">
           <h4 className="guestname">{entry.name}</h4>
           <h4 className = "guestdate">{formattedDate}</h4>
-          <div className = "guestsig">Placeholder</div>
+          <div className = "guestsig">
+            <svg 
+              viewBox="0 0 300 150"
+              width="100"
+              height="50"
+              >
+              {
+                entry.signature.map((item, index) => {
+                  return (
+                    <path
+                      d = {getSvgPathFromStroke(strokeToPath(item))}
+                      key = {index}
+                      fill = "white"
+                      stroke = "none"
+                    ></path>
+                  )
+                })}
+            </svg>
+          </div>
           <p className = "guestmessage">{entry.message}</p>
+          
           
         </div>
       )})}
