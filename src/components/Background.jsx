3179cd9f47d6ca6e1c7ff8e9098a4efa86080ec7
avin-c <avin.chiu@student.tdsb.on.ca>
2026-07-11 11:27:21 -0400

    Updated boids simulation with window event handling initial mouse event, max velocity and acceleration clamping, and seperation within radius

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -5,9 +5,22 @@ import { Application, Graphics } from 'pixi.js';
 function Background (props){
     const containerRef = useRef(null);
 
+    //pythag function
+    function magnitude(x,y){
+        return Math.sqrt(Math.pow(x,2)+Math.pow(y,2));
+    }
+
+
     useEffect(() => {
         const app =  new Application();
         let isDestroyed = false;
+
+        const mouse = { x: window.innerWidth / 2, y: window.innerHeight / 2 }; //prevent top left
+        const handleMouseMove = (e) => {
+            mouse.x = e.clientX;
+            mouse.y = e.clientY;
+        };
+        window.addEventListener("mousemove", handleMouseMove);
 
         async function initializePixiJS(){ //all pixijs setup
             
@@ -53,47 +66,74 @@ function Background (props){
                 currentFish.sprite.y = currentFish.position.y;
             }
             app.ticker.add((ticker) => {
-                let maxSpeed = 4; //max speed of fish velo
+                let maxVelocity = 4; //max speed of fish velo
+                let maxAcceleration = 0.2;
                 let seperation = 50;
-                let seperationIndex = 1.5;
+                let seperationIndex = 100;
 
-                let mouseX = app.renderer.events.pointer.global.x; //mouse position
-                let mouseY = app.renderer.events.pointer.global.y;
                 for (let i = 0; i < props.count; i++){
-                    let fish = fishArray[i];
+                    let fish = fishArray[i];    
 
-                    fish.acceleration.x = (mouseX - fish.position.x)/100;
-                    fish.acceleration.y = (mouseY - fish.position.y)/100;
+                    //ACCELERATION PART
 
-                    let steeringX = 0;
+                    //mouse steering acceleration
+                    fish.acceleration.x = (mouse.x - fish.position.x)/100;
+                    fish.acceleration.y = (mouse.y - fish.position.y)/100;
+
+                    //rest of boid's acceleration
+                    let steeringX = 0; //boid x and y vector
                     let steeringY = 0;
+
+                    let count = 0;
+
+                    //iterating over every other fish
                     for (let j = 0; j < fishArray.length; j++) {
                         if (j != i){
+
+                            ///calculate x, y, and total distance 
                             let deltaX = fishArray[i].position.x - fishArray[j].position.x;
                             let deltaY = fishArray[i].position.y - fishArray[j].position.y;
-                            let distance = Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2));
-                            if (distance < seperation){
+                            let distance = magnitude(deltaX, deltaY);
+
+                            //seperation vector added if distance is within seperation radius
+                            if (distance < seperation && distance != 0){
+                                count++;
                                 steeringX += deltaX / distance;
                                 steeringY += deltaY / distance;
-                                steeringX *= seperationIndex;
-                                steeringY *= seperationIndex;
                             }
                         }
                     }
-                    fish.acceleration.x += steeringX;
+
+                    //evens out the steering between 
+                    if (count > 0){
+                        steeringY = (steeringY / count)*seperationIndex;
+                        steeringX = (steeringX / count)*seperationIndex;
+                    }
+                    
+                    //adding boid steering logic to acceleration
+                    fish.acceleration.x += steeringX; 
                     fish.acceleration.y += steeringY;
 
-                    if (Math.abs(fish.velocity.x) < maxSpeed || Math.abs(fish.velocity.x + fish.acceleration.x) < maxSpeed ){
-                        fish.velocity.x += fish.acceleration.x;
+                    let totalAcceleration = magnitude(fish.acceleration.x, fish.acceleration.y);
+                    //clamping down acceleration
+                    if (totalAcceleration > maxAcceleration){
+                        let ratio = totalAcceleration / maxAcceleration;
+                        fish.acceleration.x = fish.acceleration.x / ratio;
+                        fish.acceleration.y /= ratio;
                     }
-                    if (Math.abs(fish.velocity.y) < maxSpeed || Math.abs(fish.velocity.y + fish.acceleration.y) < maxSpeed){
-                        fish.velocity.y += fish.acceleration.y;
+
+                    //velocity part
+                    fish.velocity.x += fish.acceleration.x;
+                    fish.velocity.y += fish.acceleration.y;
+                    let totalVelocity = magnitude(fish.velocity.x, fish.velocity.y);
+                    if (totalVelocity > maxVelocity){
+                        let ratio = totalVelocity / maxVelocity;
+                        fish.velocity.x = fish.velocity.x / ratio;
+                        fish.velocity.y = fish.velocity.y / ratio;
                     }
-                    fish.velocity.x *= 0.9; //dampen velocity to remove bounceback
-                    fish.velocity.y *= 0.9;
 
                     fish.sprite.rotation = Math.atan2(fish.velocity.y, fish.velocity.x);
-                    
+                    //position part
                     fish.position.x += fish.velocity.x;
                     fish.position.y += fish.velocity.y;
 
@@ -119,6 +159,7 @@ function Background (props){
 
         return() => {
             isDestroyed = true;
+            window.removeEventListener("mousemove", handleMouseMove);
             if (app.renderer) {
                 app.destroy({ children: true });
             }
