2537c237e53f56fb376b0c3cb14f0a00f95b9326
avin-c <avin.chiu@student.tdsb.on.ca>
2026-07-19 15:00:22 -0400

    Locked color picker on the side and fixed sizing/styling issues. Now individual colors can be chosen

--- src/components/ThemeBuilder.jsx
+++ src/components/ThemeBuilder.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useState, useEffect} from "react";
 import { ColorPicker, useColorState } from 'react-beautiful-color';
+import 'react-beautiful-color/dist/react-beautiful-color.css';
+
 function ThemeBuilder(props){
     const [colorLink, setColorLink] = useState("");
     
@@ -11,7 +13,10 @@ function ThemeBuilder(props){
         "#cbdf90",
         "#f19455"
     ]);
-    const [{ colorInput, colorState }, setColor] = useColorState({ type: 'hex', value: '#3b82f6' });
+    const [selectedColor, setSelectedColor] = useState(0);
+    const [{ colorInput, colorState }, setColor] = useColorState({ type: 'hex', value: colors[selectedColor]});
+
+    
 
     function isLinkValid (url){
         console.log(url);
@@ -153,6 +158,27 @@ function ThemeBuilder(props){
 
         return [ r * 255, g * 255, b * 255 ];
     }
+    function hsvaToHex(h, s, v, a = 1) {
+        s /= 100;
+        v /= 100;
+
+        let c = v * s;
+        let x = c * (1 - Math.abs((h / 60) % 2 - 1));
+        let m = v - c;
+
+        let r, g, b;
+
+        if (h < 60) [r,g,b] = [c,x,0];
+        else if (h < 120) [r,g,b] = [x,c,0];
+        else if (h < 180) [r,g,b] = [0,c,x];
+        else if (h < 240) [r,g,b] = [0,x,c];
+        else if (h < 300) [r,g,b] = [x,0,c];
+        else [r,g,b] = [c,0,x];
+
+        return "#" + [r,g,b]
+            .map(value => Math.round((value+m)*255).toString(16).padStart(2,"0"))
+            .join("");
+    }
     function lowerBrightness (hex){
         let rgb = hextoRGB(hex);
         let hsl = rgbToHsl(rgb[0], rgb[1], rgb[2]);
@@ -176,68 +202,72 @@ function ThemeBuilder(props){
     }, [colorLink]);
 
     useEffect(() => {
-        console.log("lastvalidlink updated");
         let colorArray = linkToArray(lastValidColorLink);
-        document.documentElement.style.setProperty(`--primary-color`, `${colorArray[0]}`); //setting primary color
-        document.documentElement.style.setProperty(`--secondary-color`, `${colorArray[1]}`);//setting secondary color
-        document.documentElement.style.setProperty(`--accent-color`, `${colorArray[2]}`);//setting accent color
-        document.documentElement.style.setProperty(`--accent-color-color`, `${colorArray[3]}`);//setting 2nd accent color
-        document.documentElement.style.setProperty('--prisecond-color', `${findMiddle(colorArray[0], colorArray[1])}`); //calculate prisecond color
-        document.documentElement.style.setProperty(`--dark-text-color`, `${lowerBrightness(colorArray[0])}`); //calculate dark text color by hex > rgb > hsl, lower l, hsl > rgb > hex
-        console.log(findMiddle(colorArray[0], colorArray[1]));
-        console.log("dark text color" + lowerBrightness(colorArray[0]));
-
-        console.log("updated css variables with " + colorArray);
+        setColors(colorArray);
     }, [lastValidColorLink]);
 
     useEffect(() => {
-    document.documentElement.style.setProperty("--primary-color", colors[0]);
-    document.documentElement.style.setProperty("--secondary-color", colors[1]);
-    document.documentElement.style.setProperty("--accent-color", colors[2]);
-    document.documentElement.style.setProperty("--accent-color-color", colors[3]);
-}, [colors]);
+        document.documentElement.style.setProperty("--primary-color", colors[0]);
+        document.documentElement.style.setProperty("--secondary-color", colors[1]);
+        document.documentElement.style.setProperty("--accent-color", colors[2]);
+        document.documentElement.style.setProperty("--accent-color-color", colors[3]);
+        document.documentElement.style.setProperty('--prisecond-color', `${findMiddle(colors[0], colors[1])}`); //calculate prisecond color
+        document.documentElement.style.setProperty(`--dark-text-color`, `${lowerBrightness(colors[0])}`); //calculate dark text color by hex > rgb > hsl, lower l, hsl > rgb > hex
+
+
+    }, [colors]);
+    useEffect(() => {
+        setColor({
+            type: "hex",
+            value: colors[selectedColor]
+        })
+    }, [selectedColor])
     return(
         <div id = {props.id} className = "content">
             <h3 className = "header">Theme Builder</h3>
-            <div className = "colorDisplay">
-            <ColorPicker color={colorInput} onChange={setColor} className = "colorPicker">
-                <ColorPicker.Saturation className="saturationPicker" />
-                    <div className="flex items-center gap-3 p-3 pt-0">
-                        <ColorPicker.EyeDropper />
-                        <div className="colorSliderContainer">
-                            <ColorPicker.Hue className="huePicker" />
-                            <ColorPicker.Alpha className="alphaPicker" />
-                        </div>
-                    </div>
-            </ColorPicker>
-                {colors.map((item, index) => {
-                    console.log(item);
-                    return (
-                        <div key = {index} className = "colorDisplayButtons">
+            <div className = "colorLayout">
+                <div className = "colorDisplay">
+                
+                    {colors.map((item, index) => {
+                        console.log(item);
+                        return (
                             <label
                                 htmlFor={`color-${index}`}
+                                key = {index}
+                                id = {`color-${index}`}
                                 style = {{backgroundColor: item}}
                                 className = "colorButton"
+                                onClick={() => setSelectedColor(index)}
                             ></label>
-                            <input
-                            className="hiddenColor"
-                            type = "color"
                             
-                            value = {item}
-                            id = {`color-${index}`}
-                            onChange={(e) => {
-                                let newColors = [...colors];
-                                newColors[index] = e.target.value;
-                                setColors(newColors);
-                            }}
-                            ></input>
+                        );
+                        })
+                        
+                    }
+                </div>
+                <ColorPicker 
+                    color={colorInput} 
+                    onChange={(newColor) => {
+                        setColor(newColor);
+                        let newColors = [...colors];
+                        newColors[selectedColor] = hsvaToHex(newColor.colorInput.h, newColor.colorInput.s, newColor.colorInput.v, newColor.colorInput.a);
+                        setColors(newColors);
+                        console.log("new color");
+                        console.log(newColor);
+                        console.log("new colors");
+                        console.log(newColors);
+                    }} 
+                    className = "colorPicker"
+                >
+                    <ColorPicker.Saturation className="saturationPicker" />
+                        <div className="colorPickerClass">
+                            <ColorPicker.EyeDropper className = "eyeDropper"/>
+                            <div className="colorSliderContainer">
+                                <ColorPicker.Hue className="color-slider" />
+                            </div>
                         </div>
-                    );
-                    })
-                    
-                }
+                </ColorPicker>
             </div>
-
             <p>Insert a <a href="https://coolors.co/322642-54426b-cbdf90-f19455">coolors.co</a> palette link to customize your viewing experience! (No guarantee of accessability/contrast)</p>
             <input
                 value = {colorLink}
@@ -247,7 +277,6 @@ function ThemeBuilder(props){
             
             <div>{isLinkValid(colorLink) ? "": "Please enter a valid link"}</div>
             
-            <div className="colorDisplay"></div>
         </div>
     );
 }
