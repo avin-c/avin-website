bc11ed7bd376177d14eed75c45f87776cc9f8a3d
avin-c <avin.chiu@student.tdsb.on.ca>
2026-07-20 14:49:47 -0400

    Changed guestbook button svg

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from "react";
-import DropdownSvg from "./svgcomponents/DropdownSvg";
+import GuestbookSvg from "./svgcomponents/GuestbookSvg";
 import Guestbook from "./Guestbook";
 function Sidebar(){
     const [isHovered, setExpand] = useState(false);
@@ -19,9 +19,9 @@ function Sidebar(){
         setExpand(true);
     }
     return (
-        <div id = "sidebar" className={`${isHovered? "sideExpand":"sideCollapse"}`} onMouseEnter={handleExpand}>
-            <div className = "sideButton" onClick={handleCollapse}>
-                <DropdownSvg upDown = {isHovered} side = {48}/>
+        <div id = "sidebar" className={`${isHovered? "sideExpand":"sideCollapse"}`} >
+            <div className = "sideButton" onClick={handleCollapse} onMouseEnter={handleExpand}>
+                <GuestbookSvg color = {"#FFFFFF"} side = {36}/>
             </div>
             <div className="sidebody">
                 <h5 className="sideTitle">Sign my guestbook!</h5>
